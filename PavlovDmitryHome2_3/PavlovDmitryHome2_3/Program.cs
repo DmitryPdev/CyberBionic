752a1f3832c8883631b33fb2b72f66926ab628a0
Dmitry <Dmitry.p.dev@gmail.com>
2019-04-12 13:57:08 +0300

    Base

---
+++ PavlovDmitryHome2_3/PavlovDmitryHome2_3/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavlovDmitryHome2_3
+{
+
+    class ClassRoom
+    {
+        private Pupil[] pupils = new Pupil[4];
+        public ClassRoom(Pupil pupils)
+        {
+            this.pupils[0] = pupils;
+        }
+        public ClassRoom(Pupil pupils1, Pupil pupils2)
+        {
+            pupils[0] = pupils1;
+            pupils[1] = pupils2;
+        }
+        public ClassRoom(Pupil pupils1, Pupil pupils2, Pupil pupils3)
+        {
+            pupils[0] = pupils1; 
+            pupils[1] = pupils2;
+            pupils[2] = pupils3;
+        }
+        public ClassRoom(Pupil pupils1, Pupil pupils2, Pupil pupils3, Pupil pupils4)
+        {
+            pupils[0] = pupils1;
+            pupils[1] = pupils2;
+            pupils[2] = pupils3;
+            pupils[3] = pupils4;
+        }
+
+        public void ViewClassInfo()
+        {
+            int i = 0;
+            while (i <  pupils.Length )
+            {
+                pupils[i].Study(pupils[i]);
+                pupils[i].Relax(pupils[i]);
+                pupils[i].Read(pupils[i]);
+                pupils[i].Write( pupils[i]);
+                Console.WriteLine(new string('-', 30));
+                i++;
+            }
+        }
+    }
+    class Pupil
+    {
+        public virtual void Study(Pupil p)
+        {
+            Console.WriteLine(p + " Pupil study");
+        }
+        public virtual void Read(Pupil p)
+        {
+            Console.WriteLine(p + " Pupil read");
+        }
+        public virtual void Write(Pupil p)
+        {
+            Console.WriteLine(p + " Pupil write");
+        }
+        public virtual void Relax(Pupil p)
+        {
+            Console.WriteLine(p + " Pupil relax");
+        }
+    }
+    class ExcelentPupil:Pupil
+    {
+        public override void Study(Pupil p)
+        {
+            Console.WriteLine(p + " ExcelentPupil study");
+        }
+        public override void Read(Pupil p)
+        {
+            Console.WriteLine(p + " ExcelentPupil read");
+        }
+        public override void Write(Pupil p)
+        {
+            Console.WriteLine(p + " ExcelentPupil write");
+        }
+        public override void Relax(Pupil p)
+        {
+            Console.WriteLine(p + " ExcelentPupil relax");
+        }
+    }
+
+    class GoodPupil :Pupil
+    {
+        public override void Study(Pupil p)
+        {
+            Console.WriteLine(p + " GoodPupil study");
+        }
+        public override void Read(Pupil p)
+        {
+            Console.WriteLine(p + " GoodPupil read");
+        }
+        public override void Write(Pupil p)
+        {
+            Console.WriteLine(p + " GoodPupil write");
+        }
+        public override void Relax(Pupil p)
+        {
+            Console.WriteLine(p + " GoodPupil relax");
+        }
+    }
+
+    class BadPupil:Pupil
+    {
+        public override void Study(Pupil p)
+        {
+            Console.WriteLine(p + " BadPupil study");
+        }
+        public override void Read(Pupil p)
+        {
+            Console.WriteLine(p + " BadPupil read");
+        }
+        public override void Write(Pupil p)
+        {
+            Console.WriteLine(p + " BadPupil write");
+        }
+        public override void Relax(Pupil p)
+        {
+            Console.WriteLine(p + " BadPupil relax");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pupil pupils = new Pupil();
+            Pupil pupils1 = new ExcelentPupil();
+            Pupil pupils2 = new BadPupil();
+            Pupil pupils3 = new GoodPupil();
+
+            ClassRoom A1 = new ClassRoom(pupils);
+            ClassRoom B1 = new ClassRoom(pupils1,pupils2);
+            ClassRoom C1 = new ClassRoom(pupils1, pupils2, pupils3);
+            ClassRoom D1 = new ClassRoom(pupils1, pupils2, pupils3,pupils);
+
+            A1.ViewClassInfo();
+            B1.ViewClassInfo();
+            C1.ViewClassInfo();
+            D1.ViewClassInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PavlovDmitryHome2_3/Three/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Three
+{
+    class Vehicle
+    {
+        public int cost { get; }
+        public int speed { get; }
+        public int yearIsue { get; }
+        public int xLocation { get; }
+        public int yLocation { get; }
+
+        public Vehicle(int xLocation,int yLocation, int cost, int speed, int yearIsue)
+        {
+            this.cost = cost;
+            this.speed = speed;
+            this.yearIsue = yearIsue;
+            this.xLocation = xLocation;
+            this.yLocation = yLocation;
+        }
+    }
+
+    class Plane : Vehicle
+    {
+        private int height;
+        private int countPassengers;
+        public Plane(int xLocation, int yLocation,int cost, int speed, int yearIsue)
+           : base(xLocation,yLocation,cost, speed, yearIsue)
+        {
+
+        }
+        public int Height
+        { get { return height; }
+          set { height = value; }
+        }
+
+        public int CountPassengers
+        {
+            get { return countPassengers; }
+            set { countPassengers = value; }
+        }
+
+        public void about()
+        {
+            Console.WriteLine("Самалёт в координаты X= {0} Y= {1}, стоимость {2}, год выпуска {5}, кол-во пасажиров {3}, высота {4}",xLocation,xLocation ,cost,countPassengers, Height,yearIsue);
+        }
+    }
+    class Ship : Vehicle
+    {
+        private int countPassengers;
+        private string port;
+        public Ship(int xLocation, int yLocation, int cost, int speed, int yearIsue)
+           : base(xLocation, yLocation, cost, speed, yearIsue)
+        {
+
+        }
+
+        public string Port
+        {
+            get { return port; }
+            set { port = value; }
+        }
+
+        public int CountPassengers
+        {
+            get { return countPassengers; }
+            set { countPassengers = value; }
+        }
+
+        public void about()
+        {
+            Console.WriteLine("Корабль в координатах X= {0} Y= {1}, стоимость {2}, год выпуска {5} кол-во пасажиров {3}, порт прибытя {4}", xLocation, xLocation, cost, countPassengers, port,yearIsue);
+        }
+    }
+    class Car : Vehicle
+    {
+        public Car(int xLocation, int yLocation, int cost, int speed, int yearIsue)
+           : base(xLocation, yLocation, cost, speed, yearIsue)
+        {
+
+        }
+
+        public void about()
+        {
+            Console.WriteLine("Машина в координатах X= {0} Y= {1}, стоимость {2}, год выпуска {3}", xLocation, xLocation, cost,yearIsue);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plane plane = new Plane(7, 2, 200000, 200, 2018);
+            plane.Height = 20;
+            plane.CountPassengers = 3;
+            Car car = new Car(2, 5, 150000, 180, 2019);
+            Ship ship = new Ship(100, 25, 4000, 250, 2018);
+            ship.CountPassengers = 4;
+            ship.Port = "First";
+
+            ship.about();
+            car.about();
+            plane.about();
+            Console.ReadKey();
+        }
+    }
+}
